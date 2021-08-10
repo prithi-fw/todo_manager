32eeafb725d849e1b202f68b280c06960d213595
prithi-fw <preethi.haridas@freshworks.com>
2021-08-10 20:54:57 +0530

    Updated database config

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 class TodosController < ApplicationController  
     def index   
-         render plain: "Hello, this is /todos!" 
+         render plain: "Hello, this is Prithi! #{DateTime.now.to_s(:short)}" 
     end
 end
+
+
