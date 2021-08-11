28c22ee1a9f1c512fa54d820b25623eceec86195
prithi-fw <preethi.haridas@freshworks.com>
2021-08-11 19:35:23 +0530

    Can see a single todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,11 @@
 class TodosController < ApplicationController  
     def index   
-         render plain: Todo.all.to_a
+         render plain: Todo.order(:due_date).map{|todo| todo.to_pleasant_string }.join("\n")
+    end
+
+    def show
+        id = params[:id]
+        render plain: "The id you're looking for is #{id}"
     end
 end
 
